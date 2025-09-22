ab03caa23f518480cfe9b828d96154c5637e399e
Raiyan Hossain <ryanhossain9797@gmail.com>
2025-09-22 20:41:08 +0600

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use osm_import::{
     FetchImportBatchRequest, FetchImportBatchResponse, PingRequest, PingResponse,
 };
 
-fn get_import_details(import_type: Option<ImportType>) -> Result<ImportOptions, String> {
+fn get_import_options(import_type: Option<ImportType>) -> Result<ImportOptions, String> {
     match import_type {
         Some(ImportType::FullDate(date)) => {
             if !date.chars().all(|c| c.is_ascii_digit()) || date.len() != 6 {
@@ -96,43 +96,42 @@ impl OsmImport for OSMImportService {
     ) -> Result<Response<FetchImportBatchResponse>, Status> {
         let req: FetchImportBatchRequest = request.into_inner();
 
-        let import_options = match get_import_details(req.import_type) {
-            Ok(details) => details,
-            Err(e) => {
-                return Ok(Response::new(FetchImportBatchResponse {
-                    response: Some(BatchResponse::Error(e)),
+        match get_import_options(req.import_type) {
+            Err(e) => Ok(Response::new(FetchImportBatchResponse {
+                response: Some(BatchResponse::Error(e)),
+            })),
+            Ok(options) => {
+                let batch_file_path =
+                    options.get_batch_file(&req.element_type, req.batch_number as usize);
+
+                let batches_complete_file_path =
+                    options.get_batches_complete_file(&req.element_type);
+
+                let maybe_existing_file = try_get_batch_file(&batch_file_path).await;
+
+                let (should_attempt_import, response) = maybe_existing_file
+                    .map(|file_result| {
+                        (
+                            false, //regardless of read success, file exists so no need to import
+                            file_result
+                                .map(BatchResponse::BatchContent) //map to batch content if read successfully
+                                .unwrap_or_else(BatchResponse::Error), //map to error if read failed
+                        )
+                    })
+                    .unwrap_or_else(|| match Path::new(&batches_complete_file_path).exists() {
+                        true => (false, BatchResponse::BatchesComplete("".to_string())), //if batches complete file exists, file would never exist so no need to import
+                        false => (true, BatchResponse::BatchesPending("".to_string())), //if not complete, we should attempt to get the file, so attempt import
+                    });
+
+                if should_attempt_import {
+                    maybe_start_background_processing(options).await;
+                }
+
+                Ok(Response::new(FetchImportBatchResponse {
+                    response: Some(response),
                 }))
             }
-        };
-
-        let batch_file =
-            import_options.get_batch_file(&req.element_type, req.batch_number as usize);
-
-        let batches_complete_file = import_options.get_batches_complete_file(&req.element_type);
-
-        let maybe_existing_file = try_get_batch_file(&batch_file).await;
-
-        let (should_attempt_import, response) = maybe_existing_file
-            .map(|file_result| {
-                (
-                    false, //regardless of read success, file exists so no need to import
-                    file_result
-                        .map(BatchResponse::BatchContent) //map to batch content if read successfully
-                        .unwrap_or_else(BatchResponse::Error), //map to error if read failed
-                )
-            })
-            .unwrap_or_else(|| match Path::new(&batches_complete_file).exists() {
-                true => (false, BatchResponse::BatchesComplete("".to_string())), //if batches complete file exists, file would never exist so no need to import
-                false => (true, BatchResponse::BatchesPending("".to_string())), //if not complete, we should attempt to get the file, so attempt import
-            });
-
-        if should_attempt_import {
-            maybe_start_background_processing(import_options).await;
         }
-
-        Ok(Response::new(FetchImportBatchResponse {
-            response: Some(response),
-        }))
     }
 }
 
